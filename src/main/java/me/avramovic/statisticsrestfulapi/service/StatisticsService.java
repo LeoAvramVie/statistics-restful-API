8ca259a04fc0b0499f3d3dcefa61a8b8fad5d8cc
ATAVRAMO_Z <ziva.avramovic@twinfaktor.at>
2021-05-04 17:40:54 +0200

    feature: add functions for Statistics2

--- src/main/java/me/avramovic/statisticsrestfulapi/service/StatisticsService.java
+++ src/main/java/me/avramovic/statisticsrestfulapi/service/StatisticsService.java
@@ -1,5 +1,6 @@
 package me.avramovic.statisticsrestfulapi.service;
 
+import lombok.RequiredArgsConstructor;
 import me.avramovic.statisticsrestfulapi.persistance.Statistics;
 import me.avramovic.statisticsrestfulapi.persistance.Transaction;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class StatisticsService {
 
     private DecimalFormat formatedValueSum = formatedValueSum = new DecimalFormat("###.##");
--- src/main/java/me/avramovic/statisticsrestfulapi/service/TransactionService.java
+++ src/main/java/me/avramovic/statisticsrestfulapi/service/TransactionService.java
@@ -1,19 +1,22 @@
 package me.avramovic.statisticsrestfulapi.service;
 
 import me.avramovic.statisticsrestfulapi.persistance.Transaction;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
+@Scope("application")
 public class TransactionService {
 
-    public static final List<Transaction> TRANSACTION_LIST = new ArrayList<>();
+    private final List<Transaction> transactionList = new LinkedList<>();
 
     public ResponseEntity addTransaction(Transaction transaction){
         if (transaction.getAmount() == null || transaction.getTimestamp() == null) {
@@ -21,11 +24,19 @@ public class TransactionService {
         } else if (transaction.getTimestamp().isAfter(ZonedDateTime.now().plusSeconds(60))) {
             return new ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY);
         } else if (transaction.getTimestamp().isBefore(ZonedDateTime.now().plusSeconds(60).truncatedTo(ChronoUnit.SECONDS))) {
-            TRANSACTION_LIST.add(transaction);
+            storeTransaction(transaction);
 
             return new ResponseEntity(HttpStatus.CREATED);
         }
             System.out.print(getTransactionList().size());
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    public void storeTransaction(Transaction transaction) {
+        transactionList.add(transaction);
+    }
+
+    public List<Transaction> getTransactionList() {
+        return transactionList;
     }
 }
